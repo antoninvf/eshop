5663e3e852288741ca03cc4859a6fd89b5a59ca3
Antonín <71491435+antoninvondrovic@users.noreply.github.com>
2021-10-04 15:35:07 +0200

    starting work on eshop

---
+++ src/com/company/Shop.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Shop {
+
+    private String name;
+    private ArrayList<Product> productList;
+    private int sales;
+
+    
+
+}
