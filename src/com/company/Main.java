b38a260fa956b9deac55a6f6f675e6db89c916af
Antonín <71491435+antoninvondrovic@users.noreply.github.com>
2021-10-04 20:50:10 +0200

    finish

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Shop czc = new Shop("czc"); // originalita, víte co
+
+        Product iphone13_128gb = new Product("iPhone 13 128GB", 22990, "Nová generace iPhone, 128GB Úložiště");
+        Product iphone13_512gb = new Product("iPhone 13 512GB", 32190, "Nová generace iPhone, 512GB Úložiště");
+        Product iphone13promax_1tb = new Product("iPhone 13 Pro Max 1TB", 47390, "Nová generace iPhone, Pro Max verze, 1TB Úložiště");
+        // neptejte se proč prodávaj jenom iphony
+
+        czc.addToStock(iphone13_128gb);
+        czc.addToStock(iphone13_512gb);
+        czc.addToStock(iphone13promax_1tb);
+
+        czc.checkAllStockCost();
+        czc.checkSales();
+
+        czc.sell(iphone13_512gb);
+
+        czc.checkAllStockCost();
+        czc.checkSales();
+
     }
 }
--- src/com/company/Shop.java
+++ src/com/company/Shop.java
@@ -5,9 +5,48 @@ import java.util.ArrayList;
 public class Shop {
 
     private String name;
-    private ArrayList<Product> productList;
+    private ArrayList<Product> productList = new ArrayList<>();
     private int sales;
+    private int allStockCost;
 
-    
+    public Shop(String name) {
+        this.name = name;
+    }
 
+    public void addToStock(Product product) {
+        productList.add(product);
+        allStockCost += product.getPrice();
+        System.out.println("[" + product.getName() + "] added to stock (Price: " + product.getPrice() + ")");
+    }
+
+    public void sell(Product product) {
+        productList.remove(product);
+        allStockCost -= product.getPrice();
+        sales += product.getPrice();
+        System.out.println("Sold [" + product.getName() + "] for " + product.getName());
+    }
+
+    public void checkAllStockCost() {
+        System.out.println("Current cost of all stock: " + getAllStockCost());
+    }
+
+    public void checkSales() {
+        System.out.println("Today's sales: " + getSales());
+    }
+
+    public int getAllStockCost() {
+        return allStockCost;
+    }
+
+    public int getSales() {
+        return sales;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
